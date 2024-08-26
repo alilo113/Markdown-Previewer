cf3b6b2c3d6d5850b0f9fdaa50bbafd27c3f240c
unknown <aliopdandan@gmail.com>
2024-08-26 16:39:12 +0100

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react"
+import React, { useState } from "react";
+import { marked } from "marked";
 
 function App() {
-  return <h1 className='text-3xl'>hello world!!</h1>
+  const [content, setContent] = useState("");
+
+  // Convert Markdown to HTML using marked
+  const getMarkdownText = () => {
+    const rawMarkup = marked(content, { sanitize: false });
+    return { __html: rawMarkup };
+  };
+
+  return (
+    <div className="bg-red-500 min-h-screen">
+      <textarea
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        className="h-60 w-full p-3 text-white bg-gray-800 mb-20"
+        placeholder="Type Markdown here..."
+      />
+      <button
+        className="bg-blue-600 text-white m-3 p-3 hover:bg-blue-800"
+      >
+        Preview
+      </button>
+      <div
+        className="p-3 bg-gray-900 text-white whitespace-pre-wrap"
+        dangerouslySetInnerHTML={getMarkdownText()}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
